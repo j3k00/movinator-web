3daf616458a8c9215c8a7b4cd1bd137952d337ff
jekoA <andreacolato94@gmail.com>
2016-10-21 01:04:21 +0200

    Comparable

--- java/intel/Juice.java
+++ java/intel/Juice.java
@@ -1,78 +1,89 @@
-public class Juice {
+public class Juice implements Comparable{
 
-    public String juice = "";
-    private String istruzione = "";
+	public String juice = "";
+	private String istruzione = "";
+	
+	
 
-    public Juice (String istruzione, String parametri) {
-        constuctJuice(istruzione, parametri);
-    }
+	public Juice (String istruzione, String parametri) {
+		constuctJuice(istruzione, parametri);
+	}
 
-    /**
-     * @param istruzione: tipo di istruzione da cui ricaveremo l'equazione
-     * @param parametri: parametri dell istruzione
-     */
-    private void constuctJuice(String istruzione,String parametri) {
-        switch (istruzione) {
-            case "mov":
-                juice = movEquations(parametri);
-        }
-    }
+	/**
+	 * @param istruzione: tipo di istruzione da cui ricaveremo l'equazione
+	 * @param parametri: parametri dell istruzione
+	 */
+	private void constuctJuice(String istruzione,String parametri) {
+		switch (istruzione) {
+			case "mov":
+				juice = movEquations(parametri);
+		}
+	}
 
-    /**
-     * @param parametri
-     * @return
-     */
-    private String movEquations(String parametri){
-        Operando op = new Operando(parametri);
-        String result = "";
+	/**
+	 * @param parametri
+	 * @return
+	 */
+	private String movEquations(String parametri){
+		Operando op = new Operando(parametri);
+		String result = "";
 
-        //
-        // TODO
-        //
-        if () {
+		//
+		// TODO
+		//
+		if () {
 
-        } else if () {
+		} else if () {
 
-        }
+		}
 
-        return result;
-    }
+		return result;
+	}
 
-    /**
-     * @param register
-     * @return
-     */
-    private String returnIdentifierRegister(String register) {
-        switch (register) {
-            case "eax":
-                return "RD1";
+	/**
+	 * @param register
+	 * @return
+	 */
+	private String returnIdentifierRegister(String register) {
+		switch (register) {
+			case "eax":
+				return "r1";
 
-            case "ebx":
-                return "RD2";
+			case "ebx":
+				return "r2";
 
-            case "ecx":
-                return "RD3";
+			case "ecx":
+				return "r3";
 
-            case "edx":
-                return "RD4";
+			case "edx":
+				return "r4";
 
-            case "eip":
-                return "RP1";
+			case "eip":
+				return "RP1";
 
-            case "esp":
-                return "RP2";
+			case "esp":
+				return "RP2";
 
-            case "ebp":
-                return "RP3";
+			case "ebp":
+				return "RP3";
 
-            case "esi":
-                return "RI1";
+			case "esi":
+				return "RI1";
 
-            case "edi":
-                return "RI2";
+			case "edi":
+				return "RI2";
 
-            default:
-                return "";
-        }
-    }
+			default:
+				return "";
+		}
+	}
+	
+	@Override
+	public int compareTo(Juice otherJuice) {       
+		
+		// calcolare log in base dieci di this e di otherJuice
+		//ritornare il risultato della sottrazione normalizzato
+		
+		return 0;
+	}
 }
