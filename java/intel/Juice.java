744740742d38098f8358b2d34edd74f36cb8bd56
jekoA <andreacolato94@gmail.com>
2016-10-21 20:11:32 +0200

    Juice

--- java/intel/Juice.java
+++ java/intel/Juice.java
@@ -1,89 +1,71 @@
 public class Juice implements Comparable{
-
-	public String juice = "";
-	private String istruzione = "";
 	
+	Semantic s = new Semantic(); // è la stringa di tipo 'i1i2s1i3'
+	long juice;
+
+	public Juice (Semantic s) {
+		this.s = s;
+		juice = squeeze();
+	}
 	
-
-	public Juice (String istruzione, String parametri) {
-		constuctJuice(istruzione, parametri);
-	}
-
-	/**
-	 * @param istruzione: tipo di istruzione da cui ricaveremo l'equazione
-	 * @param parametri: parametri dell istruzione
-	 */
-	private void constuctJuice(String istruzione,String parametri) {
-		switch (istruzione) {
-			case "mov":
-				juice = movEquations(parametri);
+	private String squeeze(); {
+		long result = 0;
+		// prendo 'i1i2s1i3'
+		// ne estraggo un numero e via di log, esempio: eax puo contenere
+		// intero
+		// char
+		// indirizzo di memoria
+		// assegno un bit per ogni rappresentazione:
+		// intero -> 00
+		// char -> 01
+		// indirizzo di memoria -> 10
+		// perchè non traduco semplicemente 'i1i2s1i3' in binario? perchè ogni char occupa 16 bit, meglio fare una cosa ad hoc (DOC -> https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html)
+		// quindi dimezzo, faccio stare 'i1' al posto che in 32 bit..in 16
+		// short -> 16 bit
+		// facciamo che useremo:
+		// 2 bit piu significativi che identificano il tipo di valore contenuto nel registo, e i 14 bit meno significativi il numero progressivo:
+		// esempio: 'i1'
+		
+		// <-tipo->|<-----------------valore------------------------------->
+		// -----------------------------------------------------------------
+		// | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 1 |
+		// -----------------------------------------------------------------
+		// esempio: 'c3'
+		// -----------------------------------------------------------------
+		// | 0 | 1 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 1 | 0 |
+		// -----------------------------------------------------------------
+		
+		new short array[];
+		
+		while (leggo due caratteri alla volta) {
+			// s è tipo 'i1'
+			short couple;
+			switch (s[0]) {
+				case 'i': 
+					couple = (short)0b0000000000000000;
+					break;
+				case 'c': 
+					couple = (short)0b0100000000000000;
+					break;
+				case 'm': 
+					couple = (short)0b1000000000000000;
+					break;
+			}
+			
+			//ora devo agganciare i successivi 14 bit, posso usare l'and
+			couple = couple | ((short)s[1] & 0b0011111111111111)s;
+			
+			//aggiungiamo all'array di short
+			array[] = couple;
 		}
-	}
-
-	/**
-	 * @param parametri
-	 * @return
-	 */
-	private String movEquations(String parametri){
-		Operando op = new Operando(parametri);
-		String result = "";
-
-		//
-		// TODO
-		//
-		if () {
-
-		} else if () {
-
-		}
-
-		return result;
-	}
-
-	/**
-	 * @param register
-	 * @return
-	 */
-	private String returnIdentifierRegister(String register) {
-		switch (register) {
-			case "eax":
-				return "r1";
-
-			case "ebx":
-				return "r2";
-
-			case "ecx":
-				return "r3";
-
-			case "edx":
-				return "r4";
-
-			case "eip":
-				return "RP1";
-
-			case "esp":
-				return "RP2";
-
-			case "ebp":
-				return "RP3";
-
-			case "esi":
-				return "RI1";
-
-			case "edi":
-				return "RI2";
-
-			default:
-				return "";
-		}
+		
+		
+		
+		return Math.log10(????);
 	}
 	
 	@Override
-	public int compareTo(Juice otherJuice) {       
-		
-		// calcolare log in base dieci di this e di otherJuice
-		//ritornare il risultato della sottrazione normalizzato
-		
-		return 0;
+	public int compareTo(Juice other) {
+		return Math.abs(this.juice - other.juice);
 	}
 }
